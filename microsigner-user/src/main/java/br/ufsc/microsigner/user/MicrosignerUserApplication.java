6af86a7f05d3d8d0dbb718a5ccc0ed76565a59b1
LucasTruppel <lucasctruppelm@gmail.com>
2023-10-04 22:31:42 -0300

    Cria microsigner-user

---
+++ microsigner-user/src/main/java/br/ufsc/microsigner/user/MicrosignerUserApplication.java
@@ -0,0 +1,13 @@
+package br.ufsc.microsigner.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrosignerUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrosignerUserApplication.class, args);
+	}
+
+}
---
+++ microsigner-user/src/test/java/br/ufsc/microsigner/user/MicrosignerUserApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufsc.microsigner.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrosignerUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
